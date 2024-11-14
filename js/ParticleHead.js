dcc0461542a3fc44d807e32ab1df7c59004ad2e0
Yudhana Nanayakkara <97881487+akshinan@users.noreply.github.com>
2024-11-14 15:51:21 +0530

    Add files via upload

---
+++ js/ParticleHead.js
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+export default function Component() {
+  return (
+    <div className="w-full h-screen bg-black flex items-center justify-center">
+      <img src="images/poe (9).webp" alt="Elephant" className="w-auto h-auto max-w-full max-h-full" />
+      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+        <h1 className="text-white text-4xl font-bold z-10">Elephant Particles</h1>
+      </div>
+    </div>
+  )
+}
